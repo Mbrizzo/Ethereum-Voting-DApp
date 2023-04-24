97670b763938808f6d6de79079d05e38ee9989bc
Marcel <mrizzo25@gmail.com>
2023-04-24 16:10:22 -0300

    conexão web3

---
+++ app.js
@@ -0,0 +1,6 @@
+// Conecta ao blockchain usando Web3
+const web3 = new Web3('http://localhost:7545'); 
+// Certifique-se que a porta 7545 no Ganache esteja aberta.
+// Se for usar infura utilize "https://mainnet.infura.io/v3/seu-ID-do-projeto" para a rede principal e lembre-se de ter eth real na sua carteira.
+// Se for usar infura na rede de testes "https://ropsten.infura.io/v3/seu_codigo_do_projeto_infura" e lembre-se de obter eth faucet em sua carteira.
+// Se for usar em outra rede, consulte o endereço correto.
