cdcfc622b4279885b34d26afa2d4d616a0cfa18c
Marcel <mrizzo25@gmail.com>
2023-04-24 16:48:23 -0300

    importação do contrato

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
+require('dotenv').config();
+
 // Conecta ao blockchain usando Web3
-const web3 = new Web3('http://localhost:7545'); 
+const web3 = new web3('http://localhost:7545'); 
 // Certifique-se que a porta 7545 no Ganache esteja aberta.
 // Se for usar infura utilize "https://mainnet.infura.io/v3/seu-ID-do-projeto" para a rede principal e lembre-se de ter eth real na sua carteira.
 // Se for usar infura na rede de testes "https://ropsten.infura.io/v3/seu_codigo_do_projeto_infura" e lembre-se de obter eth faucet em sua carteira.
 // Se for usar em outra rede, consulte o endereço correto.
+
+// Importação do contrato
+const contractAbi = JSON.parse(process.env.CONTRACT_ABI);
+const contractAddress = process.env.CONTRACT_ADDRESS;
+const contractInstance = new web3.eth.Contract(contractAbi, contractAddress);
+
