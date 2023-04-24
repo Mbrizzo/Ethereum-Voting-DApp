bf39fc1c4b2e9d22bf1fab02f5998878f6d57342
Marcel <mrizzo25@gmail.com>
2023-04-24 15:44:04 -0300

    construtor e novos métodos adicionados

--- votacao.sol
+++ votacao.sol
@@ -8,4 +8,29 @@ contract Votacao {
         string nome;
         uint256 votos;
     }
+// Mapeamento dos candidatos
+    mapping(uint256 => Candidato) public candidatos;
+
+    // Total de candidatos
+    uint256 public totalCandidatos;
+
+    // Construtor do contrato
+    constructor(string[] memory nomesCandidatos) {
+        for (uint256 i = 0; i < nomesCandidatos.length; i++) {
+            totalCandidatos++;
+            candidatos[totalCandidatos] = Candidato(nomesCandidatos[i], 0);
+        }
+    }
+
+    // Função de votação
+    function votar(uint256 candidato) public {
+        require(candidato <= totalCandidatos && candidato > 0, "candidato invalido.");
+        candidatos[candidato].votos++;
+    }
+
+    // Função de contagem de votos
+    function contagemVotos(uint256 candidato) public view returns (uint256) {
+        require(candidato <= totalCandidatos && candidato > 0, "Candidato invalido.");
+        return candidatos[candidato].votos;
+    }
 }
